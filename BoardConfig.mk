ef411257fb0bcc1bfb36d0add1ebd3cee56bb584
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-08-23 13:31:25 +0100

    zero: Inherit system.prop
    Update system.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,3 +52,6 @@ DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
 
 # Compatibility Matrix
 DEVICE_MATRIX_FILE := $(LOCAL_PATH)/compatibility_matrix.xml
+
+# Properties
+TARGET_SYSTEM_PROP += $(LOCAL_PATH)/system.prop
